3618a1fa3e9a50b1ae0a629a85da448c8783aad8
ryannekillian <64552991+ryannekillian@users.noreply.github.com>
2020-07-14 13:12:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,10 +14,15 @@
 
 // printProfileData(profileDataArgs);
 
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
+
 const generatePage = (userName, githubName) => {
   return `
     Name: ${userName}
     GitHub: ${githubName}
   `;
 };
-console.log(generatePage('Jane', 'janehub'));
+console.log(generatePage(name, github));
