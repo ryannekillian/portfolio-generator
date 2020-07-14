0724f9856663266ca826c272fd1fa347265f4ed0
ryannekillian <64552991+ryannekillian@users.noreply.github.com>
2020-07-14 12:25:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
+
 const printProfileData = profileDataArr => {
   for (let i = 0; i < profileDataArr.length; i += 1) {
     console.log(profileDataArr[i]);
   }
-};
-
-printProfileData(profileDataArgs);
 console.log('================');
 
 profileDataArr.forEach(function(profileItem) {
   console.log(profileItem);
 });
+};
+
+printProfileData(profileDataArgs);
+
