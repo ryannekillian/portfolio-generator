c09e330ba00e87746988baf94f2c6ff0f6a74953
ryannekillian <64552991+ryannekillian@users.noreply.github.com>
2020-07-14 13:04:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-console.log('================');
+// const printProfileData = profileDataArr => {
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+// console.log('================');
 
-profileDataArr.forEach(function(profileItem) {
-  console.log(profileItem);
-});
-};
+// profileDataArr.forEach(function(profileItem) {
+//   console.log(profileItem);
+// });
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
 
+const generatePage = () => 'Name: Jane, Github: janehub';
