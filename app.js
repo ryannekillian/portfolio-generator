72873771924e729fba31945928d54aa9e141e02a
ryannekillian <64552991+ryannekillian@users.noreply.github.com>
2020-07-14 13:27:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -36,5 +36,8 @@ const generatePage = (name, github) => {
   </html>
   `;
 };
-console.log(name, github);
-console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
