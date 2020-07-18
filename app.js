ce89acc1e27122cecfbc41171c53bb4d30208494
ryannekillian <ryanne.killian@gmail.com>
2020-07-18 16:09:23 -0500

    finally got it to repeat a project

--- app.js
+++ app.js
@@ -1,41 +1,14 @@
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
 
-// const printProfileData = profileDataArr => {
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-// console.log('================');
-
-// profileDataArr.forEach(function(profileItem) {
-//   console.log(profileItem);
-// });
-// };
-
-// printProfileData(profileDataArgs);
-// const fs = require('fs');
-
-// const generatePage = require('./src/page-template.js')
-
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-// const [name, github] = profileDataArgs;
-
-
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-
+DeMario Williams  4:02 PM
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
-
 const promptUser = () => {
   return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name? (Required)',
+      message: 'What is your name?',
       validate: nameInput => {
         if (nameInput) {
           return true;
@@ -53,11 +26,10 @@ const promptUser = () => {
         if (nameInput) {
           return true;
         } else {
-          console.log('Please enter your github name!');
+          console.log('Please enter your GitHub Username!');
           return false;
         }
       }
-      
     },
     {
       type: 'confirm',
@@ -72,24 +44,22 @@ const promptUser = () => {
       when: ({ confirmAbout }) => confirmAbout
     }
   ]);
-
 };
-
 const promptProject = portfolioData => {
-  // If there's no 'projects' array property, create one
+ // If there's no 'projects' array property, create one
 if (!portfolioData.projects) {
   portfolioData.projects = [];
 }
-//   console.log(`
-// =================
-// Add a New Project
-// =================
-// `);
+  console.log(`
+=================
+Add a New Project
+=================
+`);
   return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project?',
+      message: 'What is the name of your project? (Required)',
       validate: nameInput => {
         if (nameInput) {
           return true;
@@ -102,7 +72,7 @@ if (!portfolioData.projects) {
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
     },
     {
       type: 'checkbox',
@@ -134,30 +104,24 @@ if (!portfolioData.projects) {
       name: 'confirmAddProject',
       message: 'Would you like to enter another project?',
       default: false
+    } 
+  ])
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
     }
-   
-  ]);
-
+  });
 };
-
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
     console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
-
-
-
-
-// promptUser()
-//   // .then(answers => console.log(answers))
-//   .then(promptProject)
-//   .then(projectData => {
-//     portfolioData.projects.push(projectData);
-//     if (projectData.confirmAddProject) {
-//       return promptProject(portfolioData);
-//     } else {
-//       return portfolioData;
-//     }
-//   })
-  // .then(projectAnswers => console.log(projectAnswers))
